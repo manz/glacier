cabc98b1020ec524cf7f59ebb199c5690592985c
Emmanuel Peralta <eperalta@webgeoservices.com>
2014-08-02 16:33:16 +0200

    Adds last news on the home page. Allows html to be used in news content.

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,7 +3,8 @@ from restaurant.models import News
 
 
 def home(request):
-    return render_to_response('home.html', {'current': 'home'})
+    actu = News.objects.all().order_by('-posted')[0]
+    return render_to_response('home.html', {'current': 'home', 'actu': actu})
 
 
 def contact(request):
